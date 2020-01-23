46e679b038080f2ed12c0c55e1a7a7d0b2ba48a3
Shu <xx9liao@gmail.com>
2020-01-22 21:35:35 -0800

    prototype ajax

---
+++ js/predictor.js
@@ -0,0 +1,17 @@
+function get_prediction() {
+    $.ajax(
+        {
+            type: "GET",//方法类型
+            dataType: "json",//预期服务器返回的数据类型
+            async: false,
+            url: "https://9upfi662ch.execute-api.us-east-1.amazonaws.com/pred/api",//url
+            // data: $('#form1').serialize(),
+            success: function(result) {
+                document.getElementById("result").innerHTML="Predicted 20% dicount probability "
+            },
+            error : function() {     
+                alert("Some thing is wrong!");    
+           }    
+        }
+    )
+}
